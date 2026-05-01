2ce0fde2b542c3aaaeefe4348b6b15f24c4cb310
kruongartlnwsts <ongartintawong@gmail.com>
2026-05-01 16:57:26 +0700

    Delete ระบบส่งงานออนไลน์ เวอร์ชั่น firebase directory

--- ระบบส่งงานออนไลน์ เวอร์ชั่น firebase/js/supabaseConfig.js
+++
@@ -1,7 +0,0 @@
-// Supabase Configuration
-// Please replace these with your actual Supabase URL and Anon Key
-const SUPABASE_URL = 'https://gmoullxfmfauwubtruji.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdtb3VsbHhmbWZhdXd1YnRydWppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MTk5NzUsImV4cCI6MjA5MzE5NTk3NX0.6RcXFN31PEt29sMex3Oc2u8Z6po48gd9c9VnO-io684';
-
-// Initialize the Supabase Client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
